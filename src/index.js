66050b6d090372a3e03eb8fab08c778c3ceaeb4f
Timothy Batt <jammincoderguy96@gmail.com>
2023-02-11 15:35:30 -0500

    continents being displayed

--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createHashRouter, RouterProvider } from "react-router-dom";
+import Home from './pages/Home';
+
+const router = createHashRouter([
+
+	{
+		path: '/',
+        element: <Home/>
+	}
+
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   	<React.StrictMode>
-    	<App/>
+    	<RouterProvider router={router}/>
   	</React.StrictMode>
 );
 
---
+++ src/pages/Home.js
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import { capatalizeFirstLetter } from "../lib/utils";
+
+function ContinentLink({ continentName }) {
+    const continentImagePath = `/maps/${ continentName }/${ continentName }.png`;
+    return (
+        <Link to={`/${ continentName }`} className='text-center'>
+            <p className="text-lg">{ capatalizeFirstLetter(continentName) }</p> 
+            <img src={ continentImagePath }/>
+        </Link>
+    )
+    
+}
+
+export default function Home() {
+    const continents = ['africa', 'asia', 'europe', 'oceania', 'north_america', 'south_america'];
+
+    return (
+        <div className='grid place-items-center py-16'>
+            <h1 className="text-4xl mb-16">Simple Geography Game</h1>
+            <div className="grid grid-cols-3 gap-8 w-[100%] max-w-[40rem]">
+                { continents.map(continentName => ContinentLink({ continentName })) }
+            </div>
+        </div>
+    )
+}
