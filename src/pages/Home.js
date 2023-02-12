808af5b12ed3576359a58e70512489ae7e6deb61
Timothy Batt <jammincoderguy96@gmail.com>
2023-02-12 14:00:38 -0500

    improved home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,8 @@ import { createProperName, getContinentNames, resolveContinentImagePath } from "
 
 function ContinentLink({ continentName }) {
     return (
-        <Link to={`/continents/${ continentName }`} className='text-center'>
-            <p className="text-lg">{ createProperName(continentName) }</p> 
+        <Link to={`/continents/${ continentName }`} className='text-center card hover:-translate-y-1 active:-translate-y-1 transition-transform'>
+            <p className="text-lg mb-2">{ createProperName(continentName) }</p> 
             <img src={ resolveContinentImagePath(continentName) }/>
         </Link>
     )
@@ -17,9 +17,28 @@ export default function Home() {
 
     return (
         <div className='page'>
-            <h1 className="text-4xl mb-16">Simple Geography Game</h1>
-            <div className="grid grid-cols-3 gap-8 w-[100%] max-w-[40rem]">
+            <div className='text-center mb-16 grid gap-4'>
+                <h1 className="text-5xl font-bold">Simple Geography Game</h1>
+                <p>
+                    
+                    Made with <a className='link' href="https://reactjs.org/">React.js</a> and styled with <a className='link' href="https://tailwindcss.com/">Tailwind CSS</a> 
+                </p>
+            </div>
+
+            
+
+            <h2 className='text-3xl font-medium'>Pick a Continent</h2>
+            <small className='text-gray-700 mb-4'>Map sizes are not to scale.</small>
+            <div className="grid grid-cols-3 gap-8 w-[100%] max-w-[40rem] mb-16">
                 { continents.map(continentName => ContinentLink({ continentName })) }
+            </div>
+            
+            <div className='text-center'>
+                <h3 className='text-3xl'>Content Attribution</h3>
+                <p>
+                    Map icons: <a className='link' href="https://github.com/djaiss/mapsicon">https://github.com/djaiss/mapsicon</a> <br />
+                    Country ISO codes: <a className='link' href="https://gist.github.com/maephisto/9228207">https://gist.github.com/maephisto/9228207</a>
+                </p>
             </div>
         </div>
     )
