81ec016fe0e41f1cb5e17643ad11a36e71117f42
Timothy Batt <jammincoderguy96@gmail.com>
2023-02-12 12:59:00 -0500

    cleaned up path resolution code

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,13 +1,16 @@
-export function capatalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
+export const CONTINENT_MAPS_PATH = '/continents/maps';
+export const CONTINENT_JSON_PATH = '/continents/json';
 
-export function resolveCountryImagePath(continent, country) {
-    return `/continents/maps/${ continent }/${ country.iso.toLowerCase() }/vector.svg`;
-}
+
+// Paths
+export const resolveContinentImagePath = continent => `${ CONTINENT_MAPS_PATH }/${ continent }/${ continent }.png`;
+export const resolveCountryImagePath = (continent, country) => `${ CONTINENT_MAPS_PATH }/${ continent }/${ country.iso.toLowerCase() }/vector.svg`;
+export const resolveContinentJsonPath = continent => `${ CONTINENT_JSON_PATH }/${ continent }.json`;
+
+export const capatalizeFirstLetter = string => string.charAt(0).toUpperCase() + string.slice(1);
 
 export async function getCountries(continentName) {
-    const res = await fetch(`/continents/json/${ continentName }.json`);
+    const res = await fetch(resolveContinentJsonPath(continentName));
     const json = await res.json();
     return json.countries;
 }
