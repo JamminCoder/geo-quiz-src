472e0c6c98200f0242c38c0750be8a2e4233b7e1
Timothy Batt <jammincoderguy96@gmail.com>
2023-02-12 19:01:27 -0500

    navbar works

--- src/Nav.js
+++ src/Nav.js
@@ -2,14 +2,25 @@ import { NavLink } from 'react-router-dom';
 import { createProperName, getContinentNames } from './lib/utils';
 
 export default function Nav() {
+    function handleClick(e) {
+        const collapse = document.querySelector('.nav-collapse');
+        collapse.classList.toggle('hidden');
+    }
+
     return (
-        <nav className='flex gap-4 items-center bg-white shadow p-4'>
-            <NavLink to='/' className='text-xl font-medium'>Geography Quiz</NavLink>
-            <div className='flex gap-4 flex-wrap'>
-                {
-                    getContinentNames().map(continent => <NavLink key={ continent } to={ `/continents/${ continent }` } className='text-sm'>{ createProperName(continent) }</NavLink>)
-                }
+        <nav className='flex items-center justify-between bg-white shadow p-4'>
+            <div className='md:flex items-center gap-8'>
+                <NavLink to='/' className='text-xl font-medium'>Geography Quiz</NavLink>
+                <div className='nav-collapse hidden md:flex gap-4'>
+                    {
+                        getContinentNames().map(continent => <NavLink key={ continent } to={ `/continents/${ continent }` }>{ createProperName(continent) }</NavLink>)
+                    }
+                </div>
             </div>
+
+            <span onClick={ handleClick } className='md:hidden self-start'>
+                expand
+            </span>
         </nav>
     );
 }
