8b54a3ee0950df6f3c11d488b0389cc133a0311e
Timothy Batt <jammincoderguy96@gmail.com>
2023-03-18 12:38:35 -0400

    added high score

--- src/lib/GameManager.js
+++ src/lib/GameManager.js
@@ -2,7 +2,8 @@ export default class GameManager {
     static newGame() {
         const game = {
             lives: 5,
-            points: 0,   
+            points: 0,
+            highScore: GameManager.getGame().highScore || 0
         }
 
         GameManager.setGame(game);
@@ -11,7 +12,11 @@ export default class GameManager {
 
     static incrementPoints() {
         const game = GameManager.getGame();
-        game.points += 100;  
+        game.points += 100;
+        if (game.points >= game.highScore) {
+            game.highScore = game.points;
+        }
+
         GameManager.setGame(game);
     }
 
--- src/pages/GameOver.js
+++ src/pages/GameOver.js
@@ -7,7 +7,7 @@ export default function GameOver({ game, continentName }) {
         <>
             <h1 className='text-4xl font-semibold mb-4'>Game Over!</h1>
             <p className='text-xl'>Points: { game.points }</p>
-            <Link onClick={ GameManager.newGame } to={`/quiz/${ continentName }`} className='btn bg-purple-600 text-white font-medium mb-8'>Take Quiz for { createProperName(continentName) }</Link>
+            <Link onClick={ () => { GameManager.newGame(); window.location.reload(); } } to={`/quiz/${ continentName }`} className='btn bg-purple-600 text-white font-medium mb-8'>Take Quiz for { createProperName(continentName) }</Link>
             <Link to='/' className='btn bg-slate-200 font-medium mb-8'>Go to Home</Link>
         </>
     );
